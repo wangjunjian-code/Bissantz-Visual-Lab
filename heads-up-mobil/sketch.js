adbea34abdbc493e0c4fedd0739532eaf1686108
wangjunjian <111078087+wangjunjian-code@users.noreply.github.com>
2024-06-22 21:56:26 +0200

    Update

--- heads-up-mobil/sketch.js
+++ heads-up-mobil/sketch.js
@@ -185,7 +185,7 @@ function requestAccess() {
 function startPage() {
   if (gameOn === false) {
     //let rotaX = map(rotationX, -180, 180, -20, 20);
-    let rotaY = map(rotationY, -PI, PI, -100, 0);
+    let rotaY = map(rotationY, -PI, PI, 0, -10);
     rotateY(rotaY);
     // rotateX(rotaX);
     console.log("rotateY: " + rotateY + "; rotaY: " + rotaY + "; ");
