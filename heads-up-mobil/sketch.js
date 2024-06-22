76ce992bb180cb7238f850f33c8d6c892972a1aa
wangjunjian <111078087+wangjunjian-code@users.noreply.github.com>
2024-06-22 15:50:33 +0200

    Update

--- heads-up-mobil/sketch.js
+++ heads-up-mobil/sketch.js
@@ -175,13 +175,7 @@ function description(fCol, desText) {
   textAlign(CENTER, CENTER);
   textWrap(WORD);
   textLeading(height / 30);
-  text(
-    desText,
-    0,
-    height - height / 10,
-    height - height / 5,
-    width - height / 5
-  );
+  text(desText, 0, width / 2, height - height / 5, width - height / 5);
 }
 
 function computer() {
